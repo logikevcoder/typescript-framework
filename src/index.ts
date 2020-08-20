65234abbc35fd5b9e98d7aa8a9cff65010230e6b
Kevin Napier <kevin.napier@rotary.org>
2020-08-20 11:20:04 -0500

    created fetch method in User class that utilizes axios

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
+import axios from 'axios';
 import { User } from './Models/User';
 
-const user = new User({ name: 'myName', age: 33 });
+const user = new User({ id: 1 });
 
-user.set({ name: 'Kevin' });
-
-user.on('change', () => {
-  console.log('Changed #1');
-});
-
-user.on('save', () => {
-  console.log('save was called');
-});
-
-user.trigger('save');
+user.fetch();
