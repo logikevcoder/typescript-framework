d92cd83ff0c79bf255936e267fdda68a13ea9f10
Kevin Napier <kevin.napier@rotary.org>
2020-08-20 10:44:06 -0500

    added functionality for on events and trigger

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -3,7 +3,12 @@ interface UserProps {
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  // use this syntax to tell typescript we dont know yet what type of callback will be used
+  // All of the events that may be called will always point to the callback
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
@@ -12,5 +17,22 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    // guaranteed to be an array or a defined array callback eventName
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => callback());
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,13 @@ import { User } from './Models/User';
 const user = new User({ name: 'myName', age: 33 });
 
 user.set({ name: 'Kevin' });
-console.log(user.get('name'));
-console.log(user.get('age'));
+
+user.on('change', () => {
+  console.log('Changed #1');
+});
+
+user.on('save', () => {
+  console.log('save was called');
+});
+
+user.trigger('save');
